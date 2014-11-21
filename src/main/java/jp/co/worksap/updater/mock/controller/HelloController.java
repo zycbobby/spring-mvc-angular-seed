85fdc2e660889c63c020d1f99b2028371c31c64c
zuo <zuo_y@worksap.co.jp>
2014-11-21 15:53:46 +0800

    Minimize configuration with springwebmvc

--- src/main/java/jp/co/worksap/updater/mock/controller/HelloController.java
+++ src/main/java/jp/co/worksap/updater/mock/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.khalart.springmvcapp.controllers;
+package jp.co.worksap.updater.mock.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello spring webmvc app!");
